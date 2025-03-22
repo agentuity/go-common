f636cb0d2d2b74c94eeb735a52d4a7eea8033ad0
Jeff Haynie <jhaynie@gmail.com>
2025-03-22 17:51:57 -0500

    Add one-character confirmation prompt

--- tui/input.go
+++ tui/input.go
@@ -10,6 +10,7 @@ import (
 	"github.com/agentuity/go-common/logger"
 	"github.com/charmbracelet/huh"
 	"github.com/charmbracelet/x/ansi"
+	"golang.org/x/term"
 )
 
 var inputTheme = huh.ThemeBase16()
@@ -67,10 +68,12 @@ func Password(logger logger.Logger, title string, description string) string {
 	return value
 }
 
+// WaitForAnyKey prints the message "Press any key to continue..." and waits for any key press
 func WaitForAnyKey() {
 	WaitForAnyKeyMessage("Press any key to continue... ")
 }
 
+// WaitForAnyKeyMessage prints a message and waits for any key press
 func WaitForAnyKeyMessage(message string) {
 	if !HasTTY {
 		return
@@ -99,4 +102,42 @@ func WaitForAnyKeyMessage(message string) {
 			return
 		}
 	}
+}
+
+const emptyRune = byte(0)
+
+// AskForConfirm asks the user for confirmation as a single value
+func AskForConfirm(message string, defaultValue byte) byte {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		return emptyRune
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+	ch := make(chan byte, 1)
+	go func() {
+		buf := make([]byte, 1)
+		os.Stdin.Read(buf)
+		fmt.Print(ansi.CursorBackward(1)) // remove the char from the screen output
+		ch <- buf[0]
+	}()
+	fmt.Print(message)
+	select {
+	case <-ctx.Done():
+		fmt.Println()
+		os.Exit(1)
+	case answer := <-ch:
+		select {
+		case <-ctx.Done():
+			fmt.Println()
+			os.Exit(1)
+		default:
+		}
+		if answer == '\n' || answer == '\r' {
+			return defaultValue
+		}
+		return answer
+	}
+	return emptyRune
 }

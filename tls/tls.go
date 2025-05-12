ba4b2f70fa9f3e18b9abc75ca421fc4f3a552810
Jeff Haynie <jhaynie@agentuity.com>
2025-05-12 10:48:07 -0500

    tls: Return expiration with FetchTLSCert (#65)

--- tls/tls.go
+++ tls/tls.go
@@ -11,14 +11,14 @@ import (
 )
 
 // FetchTLSCert fetches a TLS certificate from aether
-func FetchTLSCert(ctx context.Context, client *redis.Client, name string) (*tls.Certificate, error) {
+func FetchTLSCert(ctx context.Context, client *redis.Client, name string) (*tls.Certificate, time.Time, error) {
 	cert, err := dns.SendDNSAction[dns.DNSCert](ctx, dns.CertRequestDNSAction(name), dns.WithRedis(client), dns.WithTimeout(time.Second*15))
 	if err != nil {
-		return nil, err
+		return nil, time.Time{}, err
 	}
 	tlsCert, err := tls.X509KeyPair(cert.Certificate, cert.PrivateKey)
 	if err != nil {
-		return nil, fmt.Errorf("error parsing tls certificate: %w", err)
+		return nil, time.Time{}, fmt.Errorf("error parsing tls certificate: %w", err)
 	}
-	return &tlsCert, nil
+	return &tlsCert, cert.Expires, nil
 }

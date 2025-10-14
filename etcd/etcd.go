26434c367365083c64b5d9082c5f6a8a73606c4e
Jeff Haynie <jhaynie@gmail.com>
2025-10-14 12:38:51 -0500

    Add etcd package

---
+++ etcd/etcd.go
@@ -0,0 +1,201 @@
+package etcd
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/namespace"
+)
+
+// EtcdClient is a simple wrapper around the etcd client
+type EtcdClient struct {
+	client *clientv3.Client
+}
+
+// Close the connection to etcd
+func (c *EtcdClient) Close() error {
+	return c.client.Close()
+}
+
+type opt struct {
+	endpoints []string
+	caCertPEM []byte
+	caKeyPEM  []byte
+	expiry    time.Duration
+	prefix    string
+}
+
+type optFun func(opt *opt)
+
+// WithEndpoints allows overriding the default etcd endpoints
+func WithEndpoints(endpoints []string) optFun {
+	return func(opt *opt) {
+		opt.endpoints = endpoints
+	}
+}
+
+// WithCA allows overriding the CA PEM
+func WithCA(caCertPEM []byte, caKeyPEM []byte) optFun {
+	return func(opt *opt) {
+		opt.caCertPEM = caCertPEM
+		opt.caKeyPEM = caKeyPEM
+	}
+}
+
+// WithPrefix allows overriding the default prefix (which is empty)
+func WithPrefix(prefix string) optFun {
+	return func(opt *opt) {
+		opt.prefix = prefix
+	}
+}
+
+// WithURL will override the default etcd endpoints with a single URL if provided (can be empty which means no override)
+func WithURL(url string) optFun {
+	return func(opt *opt) {
+		if url == "" {
+			return
+		}
+		opt.endpoints = []string{url}
+	}
+}
+
+// New creates a new EtcdClient instance with the provided options
+func New(opts ...optFun) (*EtcdClient, error) {
+	var client EtcdClient
+	var opt opt
+
+	opt.expiry = time.Hour * 24 * 30 * 12 // ~1 year
+
+	opt.endpoints = []string{
+		"https://etcd1.agentuity.com:2379",
+		"https://etcd2.agentuity.com:2379",
+		"https://etcd3.agentuity.com:2379",
+	}
+
+	if val, ok := os.LookupEnv("AGENTUITY_CA_CERTIFICATE"); ok && val != "" {
+		buf, err := base64.StdEncoding.DecodeString(val)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode CA certificate: %w", err)
+		}
+		opt.caCertPEM = buf
+	}
+
+	if val, ok := os.LookupEnv("AGENTUITY_CA_KEY"); ok && val != "" {
+		buf, err := base64.StdEncoding.DecodeString(val)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode CA key: %w", err)
+		}
+		opt.caKeyPEM = buf
+	}
+
+	for _, optfn := range opts {
+		optfn(&opt)
+	}
+
+	var tlsConfig *tls.Config
+
+	if len(opt.caCertPEM) > 0 {
+		if len(opt.caKeyPEM) == 0 {
+			return nil, fmt.Errorf("CA Cert PEM is configured but not the CA Private Key")
+		}
+		caCertPool := x509.NewCertPool()
+		if !caCertPool.AppendCertsFromPEM(opt.caCertPEM) {
+			return nil, fmt.Errorf("failed to append CA cert")
+		}
+
+		caCertBlock, _ := pem.Decode(opt.caCertPEM)
+		caCert, err := x509.ParseCertificate(caCertBlock.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse CA cert: %v", err)
+		}
+
+		caKeyBlock, _ := pem.Decode(opt.caKeyPEM)
+		caKey, err := x509.ParseECPrivateKey(caKeyBlock.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse CA key: %v", err)
+		}
+
+		// Generate client private key (EC to match CA)
+		clientKey, err := ecdsa.GenerateKey(caKey.Curve, rand.Reader)
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate client key: %v", err)
+		}
+
+		commonName := "etcd-client"
+		if opt.prefix != "" {
+			commonName = fmt.Sprintf("%s (%s)", commonName, opt.prefix)
+		}
+
+		// Create client certificate template
+		serialNumber, _ := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+		clientCertTemplate := &x509.Certificate{
+			SerialNumber: serialNumber,
+			Subject: pkix.Name{
+				CommonName:   commonName,
+				Organization: []string{"Agentuity, Inc"},
+				Country:      []string{"US"},
+			},
+			NotBefore:             time.Now(),
+			NotAfter:              time.Now().Add(opt.expiry),
+			KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+			ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+			BasicConstraintsValid: true,
+		}
+
+		// Sign the client certificate with CA
+		clientCertDER, err := x509.CreateCertificate(rand.Reader, clientCertTemplate, caCert, &clientKey.PublicKey, caKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create client cert: %v", err)
+		}
+
+		// Create TLS certificate from generated cert and key
+		clientCert, err := x509.ParseCertificate(clientCertDER)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse client cert: %v", err)
+		}
+
+		tlsCert := tls.Certificate{
+			Certificate: [][]byte{clientCertDER},
+			PrivateKey:  clientKey,
+			Leaf:        clientCert,
+		}
+
+		// Configure TLS
+		tlsConfig = &tls.Config{
+			Certificates: []tls.Certificate{tlsCert},
+			RootCAs:      caCertPool,
+			MinVersion:   tls.VersionTLS12,
+		}
+	}
+
+	// Create etcd client
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   opt.endpoints,
+		TLS:         tlsConfig,
+		DialTimeout: 5 * time.Second,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to etcd: %v", err)
+	}
+
+	if opt.prefix != "" {
+		cli.KV = namespace.NewKV(cli.KV, opt.prefix)
+		cli.Watcher = namespace.NewWatcher(cli.Watcher, opt.prefix)
+		cli.Lease = namespace.NewLease(cli.Lease, opt.prefix)
+	}
+
+	client.client = cli
+
+	return &client, nil
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.4.1
 	github.com/xhit/go-str2duration/v2 v2.1.0
 	github.com/zijiren233/yaml-comment v0.2.2
+	go.etcd.io/etcd/client/v3 v3.6.5
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.63.0
 	go.opentelemetry.io/otel v1.38.0
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.14.0
@@ -48,6 +49,8 @@ require (
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20241215232642-bb51bb14a506 // indirect
 	github.com/cockroachdb/redact v1.1.6 // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
@@ -59,6 +62,7 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -83,10 +87,14 @@ require (
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.etcd.io/etcd/api/v3 v3.6.5 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.6.5 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.38.0 // indirect
 	go.opentelemetry.io/otel/metric v1.38.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.7.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250825161204-c5933d9347a5 // indirect

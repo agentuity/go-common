12df85a39d3ccc8f7ab271d0f5b6da6dcae8f57e
Jeff Haynie <jhaynie@agentuity.com>
2025-10-17 15:42:20 -0500

    expand container environment checks (#124)
    * expand container environment checks
    
    * use rkt for rocket

--- sys/docker.go
+++ sys/docker.go
@@ -2,21 +2,37 @@ package sys
 
 import (
 	"os"
+	"regexp"
 	"strings"
 )
 
-// IsRunningInsideDocker returns true if the process is running inside a docker container.
-func IsRunningInsideDocker() bool {
+var isCgroupMatch = regexp.MustCompile("(docker|lxc|rkt|libpod|kubepods|containerd)")
+
+// IsRunningInsideContainer returns true if the process is running inside a container environment.
+func IsRunningInsideContainer() bool {
+	// Docker marks this file
 	if Exists("/.dockerenv") {
 		return true
 	}
 
+	// Podman, Docker, or CRI-O mark this file
+	if Exists("/run/.containerenv") {
+		return true
+	}
+
+	// Fallback to cgroup patterns
 	if Exists("/proc/1/cgroup") {
 		buf, _ := os.ReadFile("/proc/1/cgroup")
 		if len(buf) > 0 {
 			contents := strings.TrimSpace(string(buf))
-			return strings.Contains(contents, "docker") || strings.Contains(contents, "lxc") || strings.Contains(contents, "rt")
+			return isCgroupMatch.MatchString(contents)
 		}
 	}
 	return false
+}
+
+// IsRunningInsideDocker returns true if the process is running inside a container environment.
+// WARNING: This function is deprecated and will be removed in a future release.
+func IsRunningInsideDocker() bool {
+	return IsRunningInsideContainer()
 }
